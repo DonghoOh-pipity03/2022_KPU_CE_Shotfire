9e4359fc4d0c4cd9a1a3e6a6384f97e621bf2b29
pipity03 <87480099+DonghoOh-pipity03@users.noreply.github.com>
2022-08-20 19:49:57 +0900

    feat: SFX, Stage, proto Boss
    1. add SFX
    총소리, 장전소리, 발소리 추가
    
    2. Upgrade Stage2
    스테이지 2 사물 배치 완료
    #최적화 작업 필요
    
    3. add Proto Boss
    보스 복제

--- 2022_KPU_ShotsFired_main/Assets/Scripts/DamageableObject.cs
+++ 2022_KPU_ShotsFired_main/Assets/Scripts/DamageableObject.cs
@@ -6,10 +6,12 @@ public class DamageableObject : MonoBehaviour, IDamageable
 {
     [SerializeField] EffectKind effectKind;
     [SerializeField] bool setParent = false;    // 움직이는 물체인 경우, 데칼의 부모를 설정해주어야 함
+    [SerializeField] AudioClip[] clip_hit;    // 피격음
 
     public void ApplyDamage(DamageMessage _damageMessage){
         if(_damageMessage.damageKind == DamageKind.bullet){
             EffectManager.Instance.PlayEffect(_damageMessage.hitPoint, _damageMessage.hitNormal, effectKind, setParent?transform:null);
+            if(clip_hit.Length != 0) SoundManager.Instance.PlayLimitSFX( clip_hit[Random.Range(0, clip_hit.Length-1)], _damageMessage.hitPoint, name );
         }
     }
 }
